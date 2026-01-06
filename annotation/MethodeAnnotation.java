52505c5e5826383f87006a2b5935448022c9a00c
RASOLOFOHARILALA Sandaniaina Manoa <rasolofoharilala.s.manoa@Gmail.com>
2026-01-06 14:52:52 +0300

    Sprint6

--- annotation/MethodeAnnotation.java
+++ annotation/MethodeAnnotation.java
@@ -7,6 +7,6 @@ import java.lang.annotation.ElementType;
 // L’annotation sera visible à l’exécution et applicable aux méthodes
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public interface MethodeAnnotation {
+public @interface MethodeAnnotation {
     String value();        // ex: "/home"
 }
